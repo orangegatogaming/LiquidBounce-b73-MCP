7d51ca8cadd2bac0f132cb8ee3349a3cfaf3ff12
gato <tengosidaasdsi@gmaill.com>
2021-11-24 10:57:49 -0600

    Change Field type of fireEffect to FloatValue

--- src/minecraft/net/ccbluex/liquidbounce/features/module/modules/render/AntiBlind.java
+++ src/minecraft/net/ccbluex/liquidbounce/features/module/modules/render/AntiBlind.java
@@ -4,6 +4,7 @@ import net.ccbluex.liquidbounce.features.module.Module;
 import net.ccbluex.liquidbounce.features.module.ModuleCategory;
 import net.ccbluex.liquidbounce.features.module.ModuleInfo;
 import net.ccbluex.liquidbounce.value.BoolValue;
+import net.ccbluex.liquidbounce.value.FloatValue;
 
 @ModuleInfo(name = "AntiBlind", description = "Cancels blindness effects.", category = ModuleCategory.RENDER)
 public class AntiBlind extends Module {
@@ -20,5 +21,5 @@ public class AntiBlind extends Module {
 
 	public BoolValue confusionEffect = new BoolValue("Confusion", true);
 	public BoolValue pumpkinEffect = new BoolValue("Pumpkin", true);
-	public BoolValue fireEffect = new BoolValue("Fire", false);
+	public FloatValue fireEffect = new FloatValue("FireOpacity", 0.5f, 0, 1);
 }
