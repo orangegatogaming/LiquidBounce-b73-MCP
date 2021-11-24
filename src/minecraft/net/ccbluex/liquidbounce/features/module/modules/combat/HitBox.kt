1be9f9bdd628db303c59bd3352603142441df39d
gato <tengosidaasdsi@gmaill.com>
2021-11-23 20:59:57 -0600

    Add instance to HitBox Add instance to NoPitchLimit

--- src/minecraft/net/ccbluex/liquidbounce/features/module/modules/combat/HitBox.kt
+++ src/minecraft/net/ccbluex/liquidbounce/features/module/modules/combat/HitBox.kt
@@ -7,6 +7,13 @@ import net.ccbluex.liquidbounce.value.FloatValue
 
 @ModuleInfo(name = "HitBox", description = "Makes hitboxes of targets bigger.", category = ModuleCategory.COMBAT)
 class HitBox : Module() {
+    companion object {
+        var instance: HitBox? = null
+    }
+
+    init {
+        instance = this
+    }
 
     val sizeValue = FloatValue("Size", 0.4F, 0F, 1F)
 
