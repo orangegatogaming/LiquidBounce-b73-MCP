b5e019dbdaf892a25c3b6252529c28d275d98f6b
gato <tengosidaasdsi@gmaill.com>
2021-11-24 11:45:04 -0600

    Add instance to CameraClip

--- src/minecraft/net/ccbluex/liquidbounce/features/module/modules/render/CameraClip.java
+++ src/minecraft/net/ccbluex/liquidbounce/features/module/modules/render/CameraClip.java
@@ -6,4 +6,13 @@ import net.ccbluex.liquidbounce.features.module.ModuleInfo;
 
 @ModuleInfo(name = "CameraClip", description = "Allows you to see through walls in third person view.", category = ModuleCategory.RENDER)
 public class CameraClip extends Module {
+	private static CameraClip instance;
+
+	public static CameraClip getInstance(){
+		return instance;
+	}
+
+	public CameraClip(){
+		instance = this;
+	}
 }
