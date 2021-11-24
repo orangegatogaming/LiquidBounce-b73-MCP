1f565d6d3607687ef4c53f8e11a9ea121cb242b2
gato <tengosidaasdsi@gmaill.com>
2021-11-23 22:15:21 -0600

    Add instance to NoSwing

--- src/minecraft/net/ccbluex/liquidbounce/features/module/modules/render/NoSwing.java
+++ src/minecraft/net/ccbluex/liquidbounce/features/module/modules/render/NoSwing.java
@@ -7,5 +7,14 @@ import net.ccbluex.liquidbounce.value.BoolValue;
 
 @ModuleInfo(name = "NoSwing", description = "Disabled swing effect when hitting an entity/mining a block.", category = ModuleCategory.RENDER)
 public class NoSwing extends Module {
+	private static NoSwing instance;
+
+	public static NoSwing getInstance(){
+		return instance;
+	}
+
+	public NoSwing(){
+		instance = this;
+	}
 	public final BoolValue serverSideValue = new BoolValue("ServerSide", true);
 }
