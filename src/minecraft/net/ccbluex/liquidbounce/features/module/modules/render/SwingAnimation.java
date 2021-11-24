746b04497b8c4132a33c346cb10b48952cf3c5fb
gato <tengosidaasdsi@gmaill.com>
2021-11-24 10:54:14 -0600

    Add instance to SwingAnimation

--- src/minecraft/net/ccbluex/liquidbounce/features/module/modules/render/SwingAnimation.java
+++ src/minecraft/net/ccbluex/liquidbounce/features/module/modules/render/SwingAnimation.java
@@ -6,4 +6,13 @@ import net.ccbluex.liquidbounce.features.module.ModuleInfo;
 
 @ModuleInfo(name = "SwingAnimation", description = "Changes swing animation.", category = ModuleCategory.RENDER)
 public class SwingAnimation extends Module {
+	private static SwingAnimation instance;
+
+	public static SwingAnimation getInstance() {
+		return instance;
+	}
+
+	public SwingAnimation(){
+		instance = this;
+	}
 }
