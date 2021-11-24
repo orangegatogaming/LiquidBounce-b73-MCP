d742298357080c823bc7efc819ae4e81526d060b
gato <tengosidaasdsi@gmaill.com>
2021-11-24 12:18:34 -0600

    Add instance to Chams

--- src/minecraft/net/ccbluex/liquidbounce/features/module/modules/render/Chams.java
+++ src/minecraft/net/ccbluex/liquidbounce/features/module/modules/render/Chams.java
@@ -7,7 +7,17 @@ import net.ccbluex.liquidbounce.value.BoolValue;
 
 @ModuleInfo(name = "Chams", description = "Allows you to see targets through blocks.", category = ModuleCategory.RENDER)
 public class Chams extends Module {
-	private final BoolValue targetsValue = new BoolValue("Targets", true);
-	private final BoolValue chestsValue = new BoolValue("Chests", true);
-	private final BoolValue itemsValue = new BoolValue("Items", true);
+	private static Chams instance;
+
+	public static Chams getInstance(){
+		return instance;
+	}
+
+	public Chams(){
+		instance = this;
+	}
+
+	public final BoolValue targetsValue = new BoolValue("Targets", true);
+	public final BoolValue chestsValue = new BoolValue("Chests", true);
+	public final BoolValue itemsValue = new BoolValue("Items", true);
 }
