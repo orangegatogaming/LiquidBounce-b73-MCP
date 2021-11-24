f5694868a860349932c5ccfd4cc6fecbf9c9a251
gato <tengosidaasdsi@gmaill.com>
2021-11-23 21:07:08 -0600

    Add instance to NoJumpDelay

--- src/minecraft/net/ccbluex/liquidbounce/features/module/modules/movement/NoJumpDelay.java
+++ src/minecraft/net/ccbluex/liquidbounce/features/module/modules/movement/NoJumpDelay.java
@@ -6,4 +6,13 @@ import net.ccbluex.liquidbounce.features.module.ModuleInfo;
 
 @ModuleInfo(name = "NoJumpDelay", description = "Removes delay between jumps.", category = ModuleCategory.MOVEMENT)
 public class NoJumpDelay extends Module {
+	private static NoJumpDelay instance;
+
+	public NoJumpDelay(){
+		instance = this;
+	}
+
+	public static NoJumpDelay getInstance(){
+		return instance;
+	}
 }
