3fab058568d3ee9b8f9ffbe599fae347f1fbe85c
gato <tengosidaasdsi@gmaill.com>
2021-11-23 20:54:50 -0600

    Implement MixinAbstractClientPlayer

--- src/minecraft/net/minecraft/client/entity/AbstractClientPlayer.java
+++ src/minecraft/net/minecraft/client/entity/AbstractClientPlayer.java
@@ -1,7 +1,14 @@
 package net.minecraft.client.entity;
 
 import com.mojang.authlib.GameProfile;
+
 import java.io.File;
+import java.util.Objects;
+
+import net.ccbluex.liquidbounce.cape.CapeAPI;
+import net.ccbluex.liquidbounce.cape.CapeInfo;
+import net.ccbluex.liquidbounce.features.module.modules.misc.NameProtect;
+import net.ccbluex.liquidbounce.features.module.modules.render.NoFOV;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.network.NetworkPlayerInfo;
 import net.minecraft.client.renderer.ImageBufferDownload;
@@ -18,128 +25,145 @@ import net.minecraft.util.StringUtils;
 import net.minecraft.world.World;
 import net.minecraft.world.WorldSettings;
 
-public abstract class AbstractClientPlayer extends EntityPlayer
-{
-    private NetworkPlayerInfo playerInfo;
+public abstract class AbstractClientPlayer extends EntityPlayer {
+	private NetworkPlayerInfo playerInfo;
 
-    public AbstractClientPlayer(World worldIn, GameProfile playerProfile)
-    {
-        super(worldIn, playerProfile);
-    }
+	public AbstractClientPlayer(World worldIn, GameProfile playerProfile) {
+		super(worldIn, playerProfile);
+	}
 
-    /**
-     * Returns true if the player is in spectator mode.
-     */
-    public boolean isSpectator()
-    {
-        NetworkPlayerInfo networkplayerinfo = Minecraft.getMinecraft().getNetHandler().getPlayerInfo(this.getGameProfile().getId());
-        return networkplayerinfo != null && networkplayerinfo.getGameType() == WorldSettings.GameType.SPECTATOR;
-    }
+	/**
+	 * Returns true if the player is in spectator mode.
+	 */
+	public boolean isSpectator() {
+		NetworkPlayerInfo networkplayerinfo = Minecraft.getMinecraft().getNetHandler().getPlayerInfo(this.getGameProfile().getId());
+		return networkplayerinfo != null && networkplayerinfo.getGameType() == WorldSettings.GameType.SPECTATOR;
+	}
 
-    /**
-     * Checks if this instance of AbstractClientPlayer has any associated player data.
-     */
-    public boolean hasPlayerInfo()
-    {
-        return this.getPlayerInfo() != null;
-    }
+	/**
+	 * Checks if this instance of AbstractClientPlayer has any associated player data.
+	 */
+	public boolean hasPlayerInfo() {
+		return this.getPlayerInfo() != null;
+	}
 
-    protected NetworkPlayerInfo getPlayerInfo()
-    {
-        if (this.playerInfo == null)
-        {
-            this.playerInfo = Minecraft.getMinecraft().getNetHandler().getPlayerInfo(this.getUniqueID());
-        }
+	protected NetworkPlayerInfo getPlayerInfo() {
+		if (this.playerInfo == null) {
+			this.playerInfo = Minecraft.getMinecraft().getNetHandler().getPlayerInfo(this.getUniqueID());
+		}
 
-        return this.playerInfo;
-    }
+		return this.playerInfo;
+	}
 
-    /**
-     * Returns true if the player has an associated skin.
-     */
-    public boolean hasSkin()
-    {
-        NetworkPlayerInfo networkplayerinfo = this.getPlayerInfo();
-        return networkplayerinfo != null && networkplayerinfo.hasLocationSkin();
-    }
+	/**
+	 * Returns true if the player has an associated skin.
+	 */
+	public boolean hasSkin() {
+		NetworkPlayerInfo networkplayerinfo = this.getPlayerInfo();
+		return networkplayerinfo != null && networkplayerinfo.hasLocationSkin();
+	}
 
-    /**
-     * Returns true if the player instance has an associated skin.
-     */
-    public ResourceLocation getLocationSkin()
-    {
-        NetworkPlayerInfo networkplayerinfo = this.getPlayerInfo();
-        return networkplayerinfo == null ? DefaultPlayerSkin.getDefaultSkin(this.getUniqueID()) : networkplayerinfo.getLocationSkin();
-    }
+	/**
+	 * Returns true if the player instance has an associated skin.
+	 */
+	public ResourceLocation getLocationSkin() {
+		final NameProtect nameProtect = NameProtect.getInstance();
 
-    public ResourceLocation getLocationCape()
-    {
-        NetworkPlayerInfo networkplayerinfo = this.getPlayerInfo();
-        return networkplayerinfo == null ? null : networkplayerinfo.getLocationCape();
-    }
+		if(nameProtect.getState() && nameProtect.skinProtectValue.get()) {
+			if (nameProtect.allPlayersValue.get() && !Objects.equals(getGameProfile().getName(), Minecraft.getMinecraft().thePlayer.getGameProfile().getName()))
+				return DefaultPlayerSkin.getDefaultSkin(getUniqueID());
+		}
 
-    public static ThreadDownloadImageData getDownloadImageSkin(ResourceLocation resourceLocationIn, String username)
-    {
-        TextureManager texturemanager = Minecraft.getMinecraft().getTextureManager();
-        ITextureObject itextureobject = texturemanager.getTexture(resourceLocationIn);
+		NetworkPlayerInfo networkplayerinfo = this.getPlayerInfo();
+		return networkplayerinfo == null ? DefaultPlayerSkin.getDefaultSkin(this.getUniqueID()) : networkplayerinfo.getLocationSkin();
+	}
 
-        if (itextureobject == null)
-        {
-            itextureobject = new ThreadDownloadImageData((File)null, String.format("http://skins.minecraft.net/MinecraftSkins/%s.png", new Object[] {StringUtils.stripControlCodes(username)}), DefaultPlayerSkin.getDefaultSkin(getOfflineUUID(username)), new ImageBufferDownload());
-            texturemanager.loadTexture(resourceLocationIn, itextureobject);
-        }
+	private CapeInfo capeInfo;
 
-        return (ThreadDownloadImageData)itextureobject;
-    }
+	public ResourceLocation getLocationCape() {
+		if(CapeAPI.INSTANCE.hasCapeService()){
+			if (capeInfo == null)
+				capeInfo = CapeAPI.INSTANCE.loadCape(getUniqueID());
 
-    /**
-     * Returns true if the username has an associated skin.
-     */
-    public static ResourceLocation getLocationSkin(String username)
-    {
-        return new ResourceLocation("skins/" + StringUtils.stripControlCodes(username));
-    }
+			if(capeInfo != null && capeInfo.isCapeAvailable())
+				return capeInfo.getResourceLocation();
+		}
 
-    public String getSkinType()
-    {
-        NetworkPlayerInfo networkplayerinfo = this.getPlayerInfo();
-        return networkplayerinfo == null ? DefaultPlayerSkin.getSkinType(this.getUniqueID()) : networkplayerinfo.getSkinType();
-    }
+		NetworkPlayerInfo networkplayerinfo = this.getPlayerInfo();
+		return networkplayerinfo == null ? null : networkplayerinfo.getLocationCape();
+	}
 
-    public float getFovModifier()
-    {
-        float f = 1.0F;
+	public static ThreadDownloadImageData getDownloadImageSkin(ResourceLocation resourceLocationIn, String username) {
+		TextureManager texturemanager = Minecraft.getMinecraft().getTextureManager();
+		ITextureObject itextureobject = texturemanager.getTexture(resourceLocationIn);
 
-        if (this.capabilities.isFlying)
-        {
-            f *= 1.1F;
-        }
+		if (itextureobject == null) {
+			itextureobject = new ThreadDownloadImageData((File) null, String.format("http://skins.minecraft.net/MinecraftSkins/%s.png", new Object[]{StringUtils.stripControlCodes(username)}), DefaultPlayerSkin.getDefaultSkin(getOfflineUUID(username)), new ImageBufferDownload());
+			texturemanager.loadTexture(resourceLocationIn, itextureobject);
+		}
 
-        IAttributeInstance iattributeinstance = this.getEntityAttribute(SharedMonsterAttributes.movementSpeed);
-        f = (float)((double)f * ((iattributeinstance.getAttributeValue() / (double)this.capabilities.getWalkSpeed() + 1.0D) / 2.0D));
+		return (ThreadDownloadImageData) itextureobject;
+	}
 
-        if (this.capabilities.getWalkSpeed() == 0.0F || Float.isNaN(f) || Float.isInfinite(f))
-        {
-            f = 1.0F;
-        }
+	/**
+	 * Returns true if the username has an associated skin.
+	 */
+	public static ResourceLocation getLocationSkin(String username) {
+		return new ResourceLocation("skins/" + StringUtils.stripControlCodes(username));
+	}
 
-        if (this.isUsingItem() && this.getItemInUse().getItem() == Items.bow)
-        {
-            int i = this.getItemInUseDuration();
-            float f1 = (float)i / 20.0F;
+	public String getSkinType() {
+		NetworkPlayerInfo networkplayerinfo = this.getPlayerInfo();
+		return networkplayerinfo == null ? DefaultPlayerSkin.getSkinType(this.getUniqueID()) : networkplayerinfo.getSkinType();
+	}
 
-            if (f1 > 1.0F)
-            {
-                f1 = 1.0F;
-            }
-            else
-            {
-                f1 = f1 * f1;
-            }
+	public float getFovModifier() {
+		final NoFOV fovModule = NoFOV.Companion.getInstance();
 
-            f *= 1.0F - f1 * 0.15F;
-        }
+		if(fovModule.getState()) {
+			float newFOV = fovModule.getFovValue().get();
 
-        return f;
-    }
+			if(!this.isUsingItem()) {
+				return newFOV;
+			}
+
+			if(this.getItemInUse().getItem() != Items.bow) {
+				return newFOV;
+			}
+
+			int i = this.getItemInUseDuration();
+			float f1 = (float) i / 20.0f;
+			f1 = f1 > 1.0f ? 1.0f : f1 * f1;
+			newFOV *= 1.0f - f1 * 0.15f;
+			return newFOV;
+		}
+
+		float f = 1.0F;
+
+		if (this.capabilities.isFlying) {
+			f *= 1.1F;
+		}
+
+		IAttributeInstance iattributeinstance = this.getEntityAttribute(SharedMonsterAttributes.movementSpeed);
+		f = (float) ((double) f * ((iattributeinstance.getAttributeValue() / (double) this.capabilities.getWalkSpeed() + 1.0D) / 2.0D));
+
+		if (this.capabilities.getWalkSpeed() == 0.0F || Float.isNaN(f) || Float.isInfinite(f)) {
+			f = 1.0F;
+		}
+
+		if (this.isUsingItem() && this.getItemInUse().getItem() == Items.bow) {
+			int i = this.getItemInUseDuration();
+			float f1 = (float) i / 20.0F;
+
+			if (f1 > 1.0F) {
+				f1 = 1.0F;
+			} else {
+				f1 = f1 * f1;
+			}
+
+			f *= 1.0F - f1 * 0.15F;
+		}
+
+		return f;
+	}
 }
